6b1f347aab71264575610c560f37edde0d2b0079
jancn9 <jancn9@gmail.com>
2024-11-10 09:11:57 +1100

    feat: javascript-callbacks-and-higher-order-functions-assignment

--- ex-1.js
+++ ex-1.js
@@ -1,12 +1,24 @@
 // Exercise #1: For Each Function
 
 function forEach(array, operation) {
-  // Start coding here
+  let result = [];
+  for (let i=0; i < array.length; i++){
+  result.push(operation(array[i]))}
+  return result
+}
+
+function add(salary){
+  return salary+5000
 }
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
-const newEmployeeSalaries = [];
+let newEmployeeSalaries = [];
 
 // Using `forEach` function here
 
-console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
+
+
+newEmployeeSalaries = forEach(employeeSalaries,add)
+
+
+console.log(newEmployeeSalaries); 
--- ex-2.js
+++ ex-2.js
@@ -2,7 +2,18 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
-}
+  let countAbove70 = array.filter(operation).length;
+  if (countAbove70 >= 5){
+    return 'ผ่านเกณฑ์';
+  }else{
+    return 'ไม่ผ่านเกณฑ์'
+  }
+  }
+
+  function above70(scores){
+    return scores>70;
+  }
+
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
@@ -10,6 +21,11 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, above70);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, above70);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3,above70);
+
+
+console.log(`นักเรียนห้องที่ 1 ${(scoreRoom1Result)}`)
+console.log(`นักเรียนห้องที่ 2 ${(scoreRoom2Result)}`)
+console.log(`นักเรียนห้องที่ 3 ${(scoreRoom3Result)}`)
